8752caac2e5d70972161a5b1f9d1bc61e6cebe44
akshayb407 <akshayb407@gmail.com>
2022-09-24 15:28:43 +0530

    UC1 Employee is Present Or Absent added

---
+++ EmployeeWages.js
@@ -0,0 +1,8 @@
+//UC 1 IF ELSE Condition
+const IsABSENT = 0;
+let empCheck = Math.floor(Math.random() * 10)%2;
+if(empCheck == IsABSENT){
+    console.log("Employee is ABSENT");
+}else{
+    console.log("Employee is PRESENT");
+}
